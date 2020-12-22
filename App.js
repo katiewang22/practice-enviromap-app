f9de9a2f27848e9757e34e2c56ca91217be770e6
katiewang22 <katie.wang1950f@gmail.com>
2020-12-22 17:22:48 -0500

    get location

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, Platform } from 'react-native';
+import * as Location from 'expo-location';
 import { styles } from './AppStyles.js';
  
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location); //coords stored as var location
+    })();
+  }, []);
+
+  //Testing purposes
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>Welcome to EnviroMap! Your coords are: {text}</Text>
     </View>
   );
 }
--- AppStyles.js
+++ AppStyles.js
@@ -1,17 +1,17 @@
 import { StyleSheet } from 'react-native'
   
- const styles = StyleSheet.create({   
+const styles = StyleSheet.create({   
     container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF'
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10
-	}
-})
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+        backgroundColor: 'pink',
+      },
+      paragraph: {
+        fontSize: 18,
+        textAlign: 'center',
+      },
+});
 
 export { styles }
